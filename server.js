6a87a69ae2fcf9260d4a47eea1e6a87f7bb2537d
Karthikeya Acharya <karthikeya.acharya@gmail.com>
2024-07-16 17:45:28 +1000

    Install dotenv and configure environment variables

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const axios = require('axios');
 const moment = require('moment-timezone');
-const cors = require('cors'); // Add this line
-require('dotenv').config();
-
+const cors = require('cors');
+require('dotenv').config(); // Add this line
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Ensure environment variables are used for sensitive data
-const API_KEY = process.env.ELECTRICITYMAP_API_KEY || 't0YlBRSVRdDFF';
+app.use(cors({
+    origin: 'https://delectro.com.au' // Replace with your actual domain
+}));
 
-app.use(cors()); // Add this line
 app.use(express.static('public'));
 
 app.get('/data', async (req, res) => {
     try {
         const response = await axios.get('https://api.electricitymap.org/v3/carbon-intensity/latest', {
             headers: {
-                'auth-token': API_KEY, // Use environment variable
+                'auth-token': process.env.API_KEY, // Use the environment variable for the API key
             },
             params: {
                 zone: 'AU-NSW' // Replace with the appropriate zone identifier
@@ -32,8 +31,8 @@ app.get('/data', async (req, res) => {
         console.log('API Response:', response.data); // Log the API response
         res.json(response.data);
     } catch (error) {
-        console.error('Error retrieving data:', error.message); // Log any errors
-        res.status(500).send(`Error retrieving data: ${error.message}`);
+        console.error('Error retrieving data:', error); // Log any errors
+        res.status(500).send('Error retrieving data');
     }
 });
 
