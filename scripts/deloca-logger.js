4ee21c2ede0abf7ff41f2e0261084f4dea1699b9
KSACHA <53898199+KSACHA@users.noreply.github.com>
2025-06-04 12:20:33 +1000

    Update deloca-logger.js

--- scripts/deloca-logger.js
+++ scripts/deloca-logger.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 const SUPABASE_URL = 'https://cbfcikwyrrujufltdgsq.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiZmNpa3d5cnJ1anVmbHRkZ3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzNjA0MTgsImV4cCI6MjA1NzkzNjQxOH0.AW72XXMDipFXbvCj0ZidQuOhVS_yjEvgCmM8OJ-COyc'; // paste full key
